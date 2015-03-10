f59a4db92ad73919c00745c030f238616d0a5717
Lamb11 <abc_1118@hotmail.com>
2015-03-09 19:19:32 -0600

    Initial Commit

---
+++ UseOfParseVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UseOfParseVideo
+//
+//  Created by Alberto Cordero Arellanes on 09/03/15.
+//  Copyright (c) 2015 AlbertoCorderoArellanes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UseOfParseVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UseOfParseVideo
+//
+//  Created by Alberto Cordero Arellanes on 09/03/15.
+//  Copyright (c) 2015 AlbertoCorderoArellanes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

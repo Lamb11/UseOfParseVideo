0bc1573f61cf48dc84cb011e902e9f9689348e52
Lamb11 <abc_1118@hotmail.com>
2015-03-09 19:45:45 -0600

    Se agregaron Frameworks y se diseño la pantalla

---
+++ UseOfParseVideo/AddToParse.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UseOfParseVideo
+//
+//  Created by Alberto Cordero Arellanes on 09/03/15.
+//  Copyright (c) 2015 AlbertoCorderoArellanes. All rights reserved.
+//
+
+#import "AddToParse.h"
+
+@interface AddToParse ()
+
+@end
+
+@implementation AddToParse
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
